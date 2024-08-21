11a1dfdb9df582a3164c634664e57aecb6299fb8
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-08-21 22:26:25 +0700

    create validationError function

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -1,6 +1,7 @@
 package exception
 
 import (
+	"github.com/go-playground/validator"
 	"github.com/rizky201008/belajar-golang-restapi/helper"
 	"github.com/rizky201008/belajar-golang-restapi/model/web"
 	"net/http"
@@ -11,7 +12,30 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request, err interface{}) {
 		return
 	}
 
+	if validationError(w, r, err) {
+		return
+	}
+
 	internalServerError(w, r, err)
+}
+
+func validationError(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "Bad request",
+			Data:   exception.Error(),
+		}
+
+		helper.WriteToResponseBody(w, webResponse)
+		return true
+	} else {
+		return false
+	}
 }
 
 func notFoundError(w http.ResponseWriter, r *http.Request, err interface{}) bool {
