062a141810f5cd4209de1cbe06c574f0119a772d
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-08-22 18:39:45 +0700

    create NewRouter function

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"github.com/go-playground/validator"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 	"github.com/rizky201008/belajar-golang-restapi/app"
 	"github.com/rizky201008/belajar-golang-restapi/controller"
-	"github.com/rizky201008/belajar-golang-restapi/exception"
 	"github.com/rizky201008/belajar-golang-restapi/helper"
 	"github.com/rizky201008/belajar-golang-restapi/middleware"
 	"github.com/rizky201008/belajar-golang-restapi/repository"
@@ -21,15 +19,7 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    ":3333",
